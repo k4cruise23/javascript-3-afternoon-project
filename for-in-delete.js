2d50314004a745055abf5304989158fa96878e9b
Katie Cruise <katiecruise23@gmail.com>
2019-08-19 20:37:30 -0600

    completed 2 for-in-delete

--- for-in-delete.js
+++ for-in-delete.js
@@ -42,8 +42,9 @@
 function showValues( obj ) {
   let str = []
   for(let key in obj){
-    str.push()
+    str.push(obj[key])
   }
+  return arr.join("")
 }
 
 
@@ -58,7 +59,14 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function greaterThan10(obj){
+  for(var key in obj){
+    if(obj[key]>10){
+      obj[key]=0;
+    }
+  }
+  return obj;
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -70,7 +78,12 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function double(obj){
+  for(var key in obj){
+    obj[key] = obj[key] * 2;
+  }
+  return obj;
+}
 
 ////////// PROBLEM 4 //////////
 
